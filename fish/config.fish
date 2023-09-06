fa6b6907f642682771135815c26032d04e043935
shredderoni <shredderoni@gmail.com>
2023-09-06 21:49:37 +0300

    updated configs

--- fish/config.fish
+++ fish/config.fish
@@ -7,4 +7,5 @@ set fish_greeting
 alias 'nv' 'nvim'
 alias 'pip' '~/venv/bin/pip'
 alias 'python' '~/venv/bin/python'
+alias 'qt-designer' 'setsid ~/venv/lib/python3.11/site-packages/qt6_applications/Qt/bin/designer'
 starship init fish | source
